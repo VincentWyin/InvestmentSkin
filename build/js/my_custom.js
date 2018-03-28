adaf78fc1ea7392f922462ce6cd185715fd47136
VincentWyin <wyinwing@gmail.com>
2018-03-28 14:51:28 +0800

    Update my_custom.js

--- build/js/my_custom.js
+++ build/js/my_custom.js
@@ -3,15 +3,14 @@ $(function () {
 	
     if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
         $("#search_title .collapse-link").click();
+    	$("#menu_toggle").click();
     }
 		
-    $("#menu_toggle").click();
-	if($("#_menu .active").length == 0)
-	{
-	    console.log('reset current page');
-		setTimeout(function(){
-			var URL = window.location.href.split('#')[0].split('?')[0].split("/").pop();
-			$("#_menu").find('a[href="' + URL + '"]').parent('li').addClass('current-page').parent("ul").parent("li").addClass("active");
-		},1000);
-	}
+    if($("#_menu .active").length == 0){
+    	console.log('reset current page');
+	setTimeout(function(){
+		var URL = window.location.href.split('#')[0].split('?')[0].split("/").pop();
+		$("#_menu").find('a[href="' + URL + '"]').parent('li').addClass('current-page').parent("ul").parent("li").addClass("active");
+	},1000);
+    }
 })
