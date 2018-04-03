7712fcdb4a5bacf9af278c13426aeedbb7a28577
VincentWyin <wyinwing@zhao-vincents-iMac.local>
2018-04-03 23:34:22 +0800

    0403 update

--- build/js/my_custom.js
+++ build/js/my_custom.js
@@ -5,12 +5,12 @@ $(function () {
         $("#search_title .collapse-link").click();
     	$("#menu_toggle").click();
     }
-		
+			
     if($("#_menu .active").length == 0){
     	console.log('reset current page');
-	setTimeout(function(){
-		var URL = window.location.href.split('#')[0].split('?')[0].split("/").pop();
-		$("#_menu").find('a[href="' + URL + '"]').parent('li').addClass('current-page').parent("ul").siblings("a").click();
-	},1000);
+	    setTimeout(function(){
+		    var URL = window.location.href.split('#')[0].split('?')[0].split("/").pop();
+		    $("#_menu").find('a[href="' + URL + '"]').parent('li').addClass('current-page').parent("ul").siblings("a").click();
+	    },1000);
     }
 })
